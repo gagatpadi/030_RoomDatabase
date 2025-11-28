6893f716e1c5b1a58dd519be872d6f9be7b9376c
ASUS <padiyogaradhana@gmail.com>
2025-11-28 16:00:13 +0700

    perbaikan

--- app/src/main/java/com/example/testting/room/SiswaDao.kt
+++ app/src/main/java/com/example/testting/room/SiswaDao.kt
@@ -3,7 +3,8 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import kotlinx.coroutines.flow.Flow
+import androidx.coroutines.flow.Flow
+
 
 @Dao
 interface SiswaDao{
