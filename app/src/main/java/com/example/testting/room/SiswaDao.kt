33cf363dd89b8cfac8ade11c7b94be6b4e879718
ASUS <padiyogaradhana@gmail.com>
2025-11-28 15:52:04 +0700

    SiswaDao.kt

---
+++ app/src/main/java/com/example/testting/room/SiswaDao.kt
@@ -0,0 +1,15 @@
+package com.example.testting.room
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SiswaDao{
+    @Query("SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): List<Siswa>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+}
