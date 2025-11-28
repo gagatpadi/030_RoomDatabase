5666b8d4d03f001511f5667ecf43f0abddd2c3d5
ASUS <padiyogaradhana@gmail.com>
2025-11-28 15:51:03 +0700

    DatabaseSiswa.kt

--- app/src/main/java/com/example/testting/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/testting/room/DatabaseSiswa.kt
@@ -1,4 +1,19 @@
 package com.example.testting.room
 
-class DatabaseSiswa {
+@Database(entities = [Siswa ::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase(){
+
+    companion object{
+        @Volatile
+        private var Instance : DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa{
+            return (Instance?: synchronized(this){
+                Room.databaseBuilder(
+                    context, DatabaseSiswa::class.java,
+                    name = "siswa database")
+                    .build().also {Instance = it}
+            })
+        }
+    }
 }
